bec5d1d63bd03ef0f0a67afe18ad85f283770eb2
Digvijay Gautam <digvijay.gautam.ska@gmail.com>
2022-03-04 12:52:20 +0000

    initial commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,20 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import ReactDOM from 'react-dom';
+//import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('it renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+  //const linkElement = screen.getByText(/learn react/i);
+  //expect(linkElement).toBeInTheDocument();
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+test('it renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+  //const linkElement = screen.getByText(/learn react/i);
+  //expect(linkElement).toBeInTheDocument();
+  ReactDOM.unmountComponentAtNode(div);
 });
